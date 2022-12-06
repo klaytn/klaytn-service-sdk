ad63f652ec42a112be782f6c9647925472f111c5
naveed-klaytn <naveed.malik@krustuniverse.com>
2022-12-06 17:13:16 +0500

    multisig service added

---
+++ packages/dexs-starter-kit/services/MultiSig.ts
@@ -0,0 +1,44 @@
+import { MultiSigWallet, MultiSigWallet__factory } from '@klaytn/dex-contracts/typechain';
+import { Wallet, providers, ContractTransaction } from 'ethers'
+
+export default class MultiSig {
+    public multiSig: MultiSigWallet;
+
+    constructor(multiSigAddress: string, privKey: string, rpcURL: string) {
+        this.multiSig = MultiSigWallet__factory.connect(multiSigAddress, new Wallet(privKey, new providers.JsonRpcProvider(rpcURL)));
+    }
+
+    public async submitTransaction(destination: string, value: string, data: string): Promise<ContractTransaction> {
+        return this.multiSig.submitTransaction(
+            destination,
+            value,
+            data
+        )
+    }
+    public async confirmAndExecuteTransaction(transactionId: string): Promise<ContractTransaction> {
+        return this.multiSig.confirmAndExecuteTransaction(
+            transactionId
+        )
+    }
+    public async revokeConfirmation(transactionId: string): Promise<ContractTransaction> {
+        return this.multiSig.revokeConfirmation(
+            transactionId
+        )
+    }
+    public async executeTransaction(transactionId: string): Promise<ContractTransaction> {
+        return this.multiSig.executeTransaction(
+            transactionId
+        )
+    }
+    public async isConfirmed(transactionId: string): Promise<boolean> {
+        return this.multiSig.isConfirmed(
+            transactionId
+        )
+    }
+
+    // Getters
+
+    public getMultiSig(): MultiSigWallet {
+        return this.multiSig
+    }
+}
