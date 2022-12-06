0c88fbdb52c6146ef63409cec6a679b9e75ec671
naveed-klaytn <naveed.malik@krustuniverse.com>
2022-12-06 15:34:57 +0500

    Farming service

---
+++ packages/dexs-starter-kit/services/Farming.ts
@@ -0,0 +1,54 @@
+import { Farming as Farm, Farming__factory, DexPair, DexPair__factory } from '@klaytn/dex-contracts/typechain';
+import { Wallet, providers, ContractTransaction, BigNumber } from 'ethers'
+
+export default class Farming {
+    public farming: Farm;
+
+    constructor(routerAddress: string, factoryAddress: string, privKey: string, rpcURL: string) {
+        this.farming = Farming__factory.connect(routerAddress, new Wallet(privKey, new providers.JsonRpcProvider(rpcURL)));
+    }
+
+    public async deposit(poolId: string, amount: number): Promise<ContractTransaction> {
+        const pool = await this.farming.poolInfo(poolId);
+        const signerAddress: string = await this.farming.signer.getAddress();
+        let lp:DexPair = new DexPair__factory().attach(pool.lpToken);
+        if((await lp.balanceOf(signerAddress)).lt(BigNumber.from(amount))) throw new Error('func#deposit LP balance insufficient');
+        if((await lp.allowance(signerAddress, this.farming.address)).lt(BigNumber.from(amount))) throw new Error('func#deposit LP allowance insufficient');
+        return this.farming.deposit(poolId, amount);
+
+    }
+    public async withdraw(poolId: string, amount: number): Promise<ContractTransaction> {
+        const signerAddress: string = await this.farming.signer.getAddress();
+        const user: [BigNumber, BigNumber] & { amount: BigNumber; rewardDebt: BigNumber } = await this.farming.userInfo(poolId, signerAddress);
+        // check if given amount is valid
+        if((user.amount).lt(BigNumber.from(amount))) throw new Error('func#withdraw deposited amount < withdrawing amount');
+
+        return this.farming.withdraw(poolId, amount);
+
+    }
+    public async emergencyWithdraw(poolId: string): Promise<ContractTransaction> {
+        const signerAddress: string = await this.farming.signer.getAddress();
+        const user: [BigNumber, BigNumber] & { amount: BigNumber; rewardDebt: BigNumber } = await this.farming.userInfo(poolId, signerAddress);
+        // check if given amount is valid
+        if((user.amount).gt(BigNumber.from(0))) throw new Error('func#emergencyWithdraw deposited LP not found');
+
+        return this.farming.emergencyWithdraw(poolId);
+
+    }
+    public async getPendingReward(poolId: string): Promise<BigNumber> {
+        const signerAddress: string = await this.farming.signer.getAddress();
+        const user: [BigNumber, BigNumber] & { amount: BigNumber; rewardDebt: BigNumber } = await this.farming.userInfo(poolId, signerAddress);
+        // check if given amount is valid
+        if((user.amount).gt(BigNumber.from(0))) throw new Error('func#getPendingReward deposited LP not found');
+
+        return this.farming.pendingPtn(poolId, signerAddress);
+
+    }
+
+    public getFarm(): Farm {
+        return this.farming
+    }
+    public async getPair(pairAddress: string, privKey: string, rpcURL: string):Promise<DexPair> {
+        return DexPair__factory.connect(pairAddress, new Wallet(privKey, new providers.JsonRpcProvider(rpcURL)))
+    }
+}
