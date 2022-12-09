fff8619f1272354f0bb63a435f72daeca846d254
naveed-klaytn <naveed.malik@krustuniverse.com>
2022-12-09 15:54:18 +0500

    example/multisig/submitTransaction

---
+++ packages/dexs-starter-kit/examples/Multisig/submitTransaction.ts
@@ -0,0 +1,30 @@
+import { MultiSig} from "../../services"
+import { KIP7__factory, KIP7 } from '../../contracts';
+import { Wallet, providers, BigNumber, constants } from 'ethers'
+import { config } from 'dotenv'
+config()
+;( async ()=> {
+    console.log('submitTransaction# initiating...')
+    const rpcURL = process.env.RPC_URL!
+    const privKey = process.env.PRIVATE_KEY!
+    const pubKey = process.env.PUBLIC_KEY!
+    const multisigAddress = process.env.MULTISIG!
+    const targetContract = process.env.DESTINATION!
+    const rawTx = process.env.DATA!
+
+    console.log('submitTransaction# Multisig => setting up')
+    const multiSig = new MultiSig(multisigAddress, privKey, rpcURL);
+    console.log('submitTransaction# Multisig => owner => checking')
+    const owners = await multiSig.multiSig.getOwners()
+    if (!owners.includes( pubKey)) throw new Error('submitTransaction# Multisig => owner => signer not an owner');
+    console.log('submitTransaction# Multisig => owner => valid')
+
+    console.log('submitTransaction# Multisig => Transaction => preparing')
+    const submitTx = await multiSig.submitTransaction(targetContract, '0', rawTx);
+    console.log('submitTransaction# Multisig => Transaction => txHash: ' + submitTx.hash)
+    console.log('submitTransaction# Multisig => Transaction => waiting for confirmations')
+    await submitTx.wait(parseInt(process.env.CONFIRMATIONS!) || 6)
+    console.log('submitTransaction# Multisig => Transaction => confirmed')
+    console.log('submitTransaction# Multisig => DONE')
+
+})()
