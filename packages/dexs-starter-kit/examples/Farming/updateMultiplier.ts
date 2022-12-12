c1de068df1ad995c62071b2d4bbf1ea1fa5bae2b
naveed-klaytn <naveed.malik@krustuniverse.com>
2022-12-12 20:44:27 +0500

    example/farming/updateMultiplier

---
+++ packages/dexs-starter-kit/examples/Farming/updateMultiplier.ts
@@ -0,0 +1,25 @@
+import { Farming} from "../../services"
+import { KIP7__factory, KIP7 } from '../../contracts';
+import { Wallet, providers, BigNumber, constants } from 'ethers'
+import { config } from 'dotenv'
+config()
+;( async ()=> {
+    console.log('updateMultiplier# initiating...')
+    const rpcURL = process.env.RPC_URL!
+    const privKey = process.env.PRIVATE_KEY!
+    const pubKey = process.env.PUBLIC_KEY!
+    const farmingAddress = process.env.FARMING!
+    const bonusMultiplier = process.env.BONUS_MULTIPLIER!
+    const poolId = process.env.POOL_ID!
+
+    console.log('updateMultiplier# Farming => setting up')
+    if (!(rpcURL && privKey && farmingAddress && bonusMultiplier && poolId)) {
+        throw new Error('updateMultiplier# ERROR => some .evn variables are missing!')
+    }
+    const farming = new Farming(farmingAddress, privKey, rpcURL);
+    console.log('updateMultiplier# Farming => Transaction => encoding')
+    const rawTx = await farming.updateMultiplier(poolId, bonusMultiplier)
+    console.log('updateMultiplier# Farming => Transaction => ready to submit on MultiSig')
+    console.log('updateMultiplier# Farming => Transaction => encoded data =>', rawTx)
+
+})()
