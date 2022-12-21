e021b496fe75ca81ada12a3b0d941be37b619fdf
naveed-klaytn <naveed.malik@krustuniverse.com>
2022-12-21 19:47:15 +0500

    core/Swap.ts jsDoc added

--- packages/dexs-starter-kit/core/Swap.ts
+++ packages/dexs-starter-kit/core/Swap.ts
@@ -10,6 +10,14 @@ export class Swap {
         this.router = DexRouter__factory.connect(routerAddress, new Wallet(privKey, new providers.JsonRpcProvider(rpcURL)));
     }
 
+    /**
+     * A function to swap exact amount of Tokens for a given amount of Tokens.
+     * @param {string[]} path - a pair of tokens addresses, path[0] should be the address of input Token & path[1] should be the address of output Token.
+     * @param {string} amountIn- amount of Token to be swapped.
+     * @param {string} amountDesiredOut- minimum amount of Tokens expecting to receive.
+     * @param {number} deadline - UTC timestamp as deadline of this transaction, once deadline passed on-chain transaction should be reverted.
+     * * @return {Promise<ContractTransaction>} - ContractTransaction object.
+     */
     public async exactTokensForTokens(amountIn: string, amountDesiredOut: string, path: string[], deadline: string): Promise<ContractTransaction> {
         if (path.length < 2) throw new Error('exactTokensForTokens => path length must be at least 2');
         const inputToken: KIP7 = KIP7__factory.connect(path[0], this.router.provider);
@@ -20,6 +28,15 @@ export class Swap {
 
         /*const tx: ContractTransaction = await*/ return this.router.swapExactTokensForTokens(amountIn, amountDesiredOut, path, signerAddress, deadline);
     }
+
+    /**
+     * A function to swap given amount of Tokens for exact amount of KLAYs.
+     * @param {string[]} path - a pair of tokens addresses, path[0] should be the address of Token & path[1] should be the address of WKLAY.
+     * @param {string} amountDesiredMaxIn- max amount of Tokens to be swapped.
+     * @param {string} amountOut- exact amount of KLAYs expecting to receive.
+     * @param {number} deadline - UTC timestamp as deadline of this transaction, once deadline passed on-chain transaction should be reverted.
+     * @return {Promise<ContractTransaction>} - ContractTransaction object.
+     */
     public async tokensForExactTokens(amountOut: string, amountDesiredMaxIn: string, path: string[], deadline: string): Promise<ContractTransaction> {
         if (path.length < 2) throw new Error('tokensForExactTokens => path length must be at least 2');
         const inputToken: KIP7 = KIP7__factory.connect(path[0], this.router.provider);
@@ -30,6 +47,15 @@ export class Swap {
 
         /*const tx: ContractTransaction = await*/ return this.router.swapTokensForExactTokens(amountOut, amountDesiredMaxIn, path, signerAddress, deadline);
     }
+
+    /**
+     * A function to swap exact amount of KLAY for a given amount of Tokens.
+     * @param {string[]} path - a pair of tokens address, path[0] should be the address of WKLAY & path[1] should be the address of out Token.
+     * @param {string} amountKlayIn- amount of WKLAY tokens to be swapped.
+     * @param {string} amountDesiredOutMin- minimum amount of tokens expecting to receive.
+     * @param {number} deadline - UTC timestamp as deadline of this transaction, once deadline passed on-chain transaction should be reverted.
+     * @return {Promise<ContractTransaction>} - ContractTransaction object.
+     */
     public async exactKlayForTokens(amountKlayIn: string, amountDesiredOutMin: string, path: string[], deadline: string): Promise<ContractTransaction> {
         if (path.length < 2) throw new Error('func#exactKlayForTokens: path length must be at least 2');
         // const inputToken: KIP7 = new KIP7__factory().attach(path[0]); // WKLAY no approval required as router internally wrapping KLAY into WKLAY to proceed
@@ -41,6 +67,14 @@ export class Swap {
         /*const tx: ContractTransaction = await*/ return this.router.swapExactKLAYForTokens(amountDesiredOutMin, path, signerAddress, deadline, {value: amountKlayIn});
     }
 
+    /**
+     * A function to swap given amount of Tokens for exact amount of KLAYs.
+     * @param {string[]} path - a pair of tokens addresses, path[0] should be the address of Token & path[1] should be the address of WKLAY.
+     * @param {string} amountDesiredTokenMaxIn- max amount of Tokens to be swapped.
+     * @param {string} amountKlayOut- exact amount of KLAYs expecting to receive.
+     * @param {number} deadline - UTC timestamp as deadline of this transaction, once deadline passed on-chain transaction should be reverted.
+     * @return {Promise<ContractTransaction>} - ContractTransaction object.
+     */
     public async tokensForExactKlay(amountKlayOut: string, amountDesiredTokenMaxIn: string, path: string[], deadline: string): Promise<ContractTransaction> {
         if (path.length < 2) throw new Error('tokensForExactTokens => path length must be at least 2');
         const inputToken: KIP7 = KIP7__factory.connect(path[0], this.router.provider);
@@ -51,6 +85,16 @@ export class Swap {
 
         /*const tx: ContractTransaction = await*/ return this.router.swapTokensForExactKLAY(amountKlayOut, amountDesiredTokenMaxIn, path, signerAddress, deadline);
     }
+
+
+    /**
+     * A function to swap exact amount of Tokens for a given amount of KLAY.
+     * @param {string[]} path - a pair of tokens addresses, path[0] should be the address of Token & path[1] should be the address of WKLAY.
+     * @param {string} amountIn- amount of Token to be swapped.
+     * @param {string} amountDesiredKlayOut- minimum amount of KLAYs expecting to receive.
+     * @param {number} deadline - UTC timestamp as deadline of this transaction, once deadline passed on-chain transaction should be reverted.
+     * @return {Promise<ContractTransaction>} - ContractTransaction object.
+     */
     public async exactTokensForKlay(amountIn: string, amountDesiredKlayOut: string, path: string[], deadline: string): Promise<ContractTransaction> {
         if (path.length < 2) throw new Error('exactTokensForTokens => path length must be at least 2');
         const inputToken: KIP7 = KIP7__factory.connect(path[0], this.router.provider);
@@ -61,6 +105,15 @@ export class Swap {
 
         /*const tx: ContractTransaction = await*/ return this.router.swapExactTokensForTokens(amountIn, amountDesiredKlayOut, path, signerAddress, deadline);
     }
+
+    /**
+     * A function to swap KLAYs for a given exact amount of Tokens.
+     * @param {string[]} path - a pair of tokens addresses, path[0] should be the address of WKLAY & path[1] should be the address of output Token.
+     * @param {string} amountKlayIn- max amount of WKLAY to be swapped.
+     * @param {string} amountDesiredOut- exact amount of Tokens expecting to receive.
+     * @param {number} deadline - UTC timestamp as deadline of this transaction, once deadline passed on-chain transaction should be reverted.
+     * @return {Promise<ContractTransaction>} - ContractTransaction object.
+     */
     public async klayForExactTokens(amountKlayIn: string, amountDesiredOut: string, path: string[], deadline: string): Promise<ContractTransaction> {
         if (path.length < 2) throw new Error('klayForExactTokens => path length must be at least 2');
         // const inputToken: KIP7 = new KIP7__factory().attach(path[0]); // WKLAY no approval required as router internally wrapping KLAY into WKLAY to proceed
@@ -73,12 +126,28 @@ export class Swap {
     }
 
     // Getters
-
+    /**
+     * A getter function to return DexRouter contract's instance
+     * @return {DexRouter} - DexRouter contract's instance.
+     */
     public getRouter(): DexRouter { return this.router}
+    /**
+     * A getter function to fetch instance of DexPair contract.
+     * @param {string} tokenA - Address of token0 contract.
+     * @param {string} tokenB - Address of token1 contract.
+     * @param {string} privKey - private key of signer account.
+     * @param {string} rpcURL - RPC URL of blockchain provider.
+     * @return {Promise<DexPair>} - DexPair contract's instance.
+     */
     public async getPair(tokenA: string, tokenB: string, privKey: string, rpcURL: string):Promise<DexPair> {
         const pairAddress: string = await this.factory.getPair(tokenA, tokenB)
         return DexPair__factory.connect(pairAddress, new Wallet(privKey, new providers.JsonRpcProvider(rpcURL)))
     }
+
+    /**
+     * A getter function to get WKLAY contract's address
+     * @return {string} - WKLAY address.
+     */
     public async getAddressOfWKLAY():Promise<string> {
         return this.router.WKLAY()
     }
