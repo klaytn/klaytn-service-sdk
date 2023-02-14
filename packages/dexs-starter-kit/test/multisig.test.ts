7eb716a145a2d5fc88740e5ba32fbe920228c56c
toniya-klaytn <100742846+toniya-klaytn@users.noreply.github.com>
2023-02-14 14:35:34 +0800

    added swap test

---
+++ packages/dexs-starter-kit/test/multisig.test.ts
@@ -0,0 +1,97 @@
+import { MultiSigWallet, MultiSigWallet__factory } from '../contracts';
+import { Wallet, providers, ContractTransaction } from 'ethers';
+import { MultiSig } from '../core/MultiSig';
+
+
+jest.mock('ethers', () => ({
+  Wallet: jest.fn().mockImplementation(() => ({})),
+
+  providers: {
+    JsonRpcProvider: jest.fn().mockImplementation(() => ({}))
+  }
+}))
+
+
+jest.mock('../contracts', () => {
+  const mockMultiSigWallet = {
+      submitTransaction: jest.fn(),
+      confirmAndExecuteTransaction: jest.fn(),
+      revokeConfirmation: jest.fn(),
+      executeTransaction: jest.fn(),
+      isConfirmed: jest.fn()
+  };
+  return {
+      MultiSigWallet: jest.fn(() => mockMultiSigWallet),
+      MultiSigWallet__factory: {
+          connect: jest.fn(() => mockMultiSigWallet)
+      }
+  };
+});
+
+describe('MultiSig', () => {
+  let multiSig: MultiSig;
+  const multiSigAddress = process.env.MULTISIG_ADDRESS!;
+  const privKey = process.env.PRIVATE_KEY!;
+  const rpcURL = process.env.RPC_URL!;
+
+    beforeEach(() => {
+        multiSig = new MultiSig(multiSigAddress, privKey, rpcURL);
+    });
+
+    describe('submitTransaction', () => {
+      it('should call submitTransaction method on the MultiSigWallet contract', async () => {
+          const destination = multiSigAddress;
+          const value = '0';
+          const data = '0xc6427474000000000000000000000000';
+          const mockResult = {};
+
+          (multiSig.multiSig.submitTransaction as jest.Mock).mockResolvedValue(mockResult);
+
+          const result = await multiSig.submitTransaction(destination, value, data);
+
+          expect(result).toEqual(mockResult);
+          expect(multiSig.multiSig.submitTransaction).toHaveBeenCalledWith(destination, value, data);
+      });
+    });
+
+    describe('confirmAndExecuteTransaction', () => {
+      it('should call confirmAndExecuteTransaction method on the MultiSigWallet contract', async () => {
+          const transactionId = '0x1234';
+          const mockResult = {};
+
+          (multiSig.multiSig.confirmAndExecuteTransaction as jest.Mock).mockResolvedValue(mockResult);
+
+          const result = await multiSig.confirmAndExecuteTransaction(transactionId);
+
+          expect(result).toEqual(mockResult);
+          expect(multiSig.multiSig.confirmAndExecuteTransaction).toHaveBeenCalledWith(transactionId, { gasLimit: 200000 });
+      });
+    });
+
+    describe('confirmAndExecuteTransaction', () => {
+      it('should call revokeConfirmation method on the MultiSigWallet contract', async () => {
+        const transactionId = '0x1234';
+        const mockResult = {};
+
+        (multiSig.multiSig.revokeConfirmation as jest.Mock).mockResolvedValue(mockResult);
+
+        const result = await multiSig.revokeConfirmation(transactionId);
+
+        expect(result).toEqual(mockResult);
+        expect(multiSig.multiSig.revokeConfirmation).toHaveBeenCalled
+      });
+    });
+
+    describe('isConfirmed', () => {
+      it("should call isConfirmed on the MultiSigWallet contract", async () => {
+        const transactionId = '0x1234';
+        const mockResult = {};
+
+        (multiSig.multiSig.isConfirmed as jest.Mock).mockResolvedValue(mockResult);
+
+        const result = await multiSig.isConfirmed(transactionId);
+        expect(result).toEqual(mockResult);
+        expect(multiSig.multiSig.isConfirmed).toHaveBeenCalled
+      })
+    })
+})
--- packages/dexs-starter-kit/use-cases/Multisig/submitTransaction.ts
+++ packages/dexs-starter-kit/use-cases/Multisig/submitTransaction.ts
@@ -6,7 +6,6 @@ import { ContractReceipt } from 'ethers'
  * @notice only registered owner on MULTISIG contract & who has already voted the given transactionId can execute this function
  * @param {string} rpcURL - RPC URL of blockchain provider.
  * @param {string} privKey - secret key of account with which you want to sign the transaction.
- * @param {string} pubKey - public key / address of account with which you want to sign the transaction.
  * @param {string} multisigAddress - MULTISIG contract's address.
  * @param {string} targetContract - the contract on which given rawTx to be executed (once got enough votes).
  * @param {string} rawTx - the encoded rawTx data which is required to be submitted.
@@ -16,7 +15,6 @@ import { ContractReceipt } from 'ethers'
 export async function submitTransaction(
     rpcURL: string,
     privKey: string,
-    pubKey: string,
     multisigAddress: string,
     targetContract: string,
     rawTx: string,
@@ -35,7 +33,7 @@ export async function submitTransaction(
     const submitTx = await multiSig.submitTransaction(targetContract, '0', rawTx);
     console.log('submitTransaction# Multisig => Transaction => txHash: ' + submitTx.hash)
     console.log('submitTransaction# Multisig => Transaction => waiting for confirmations')
-    const receipt = await submitTx.wait(confirmations || 6)
+    const receipt = await submitTx.wait(confirmations || 2)
     console.log('submitTransaction# Multisig => Transaction => confirmed')
     console.log('submitTransaction# Multisig => DONE')
     return receipt;
